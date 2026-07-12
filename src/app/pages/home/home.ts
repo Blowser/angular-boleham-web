ee0377f5a8acf10ff59301e21fb8b2e247230b67
Ignacio Cortez <ig.cortez@duocuc.cl>
2026-07-11 22:49:25 -0400

    Update home.ts
    PREVENTA EN HOME

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule, DecimalPipe } from '@angular/common';
+
+import { ProductosService } from '../../services/productos.service';
+import { Producto } from '../../models/producto.model';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    CommonModule,   // ← Necesario para *ngFor y *ngIf
+    DecimalPipe     // ← Necesario para el pipe number
+  ],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
-export class Home {}
+export class Home implements OnInit {
+
+  preventas: Producto[] = [];
+
+  constructor(private productosService: ProductosService) {}
+
+  ngOnInit(): void {
+    this.productosService.obtenerProductos().subscribe((data: Producto[]) => {
+      this.preventas = data.filter((p: Producto) => p.categoria === 'Preventas');
+    });
+  }
+}
