d87eb3f8dc97735fe27af98699158265d616ce71
Ignacio Cortez <ig.cortez@duocuc.cl>
2026-07-12 19:41:03 -0400

    Update productos.service.ts
    CRUD ADMIN

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,8 +9,29 @@ export class ProductosService {
   private readonly apiUrl = 'http://localhost:3000/productos';
 
   constructor(private http: HttpClient) {}
-
+  
+// READ: todos
   obtenerProductos(): Observable<Producto[]> {
     return this.http.get<Producto[]>(this.apiUrl);
   }
+// READ: uno
+obtenerProducto(id: number): Observable<Producto> {
+  return this.http.get<Producto>(`${this.apiUrl}/${id}`);
+}
+
+// CREATE
+crearProducto(producto: Producto): Observable<Producto> {
+  return this.http.post<Producto>(this.apiUrl, producto);
+}
+
+// UPDATE
+actualizarProducto(id: number, producto: Producto): Observable<Producto> {
+  return this.http.put<Producto>(`${this.apiUrl}/${id}`, producto);
+}
+
+// DELETE
+eliminarProducto(id: number): Observable<void> {
+  return this.http.delete<void>(`${this.apiUrl}/${id}`);
+}
+
 }
