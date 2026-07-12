126f2cf25545ef8ead3aef50d7da30937bda2e6a
Ignacio Cortez <ig.cortez@duocuc.cl>
2026-07-11 22:49:21 -0400

    Create productos.service.ts
    PREVENTA EN HOME

---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Producto } from '../models/producto.model';
+
+@Injectable({ providedIn: 'root' })
+export class ProductosService {
+
+  private readonly apiUrl = 'http://localhost:3000/productos';
+
+  constructor(private http: HttpClient) {}
+
+  obtenerProductos(): Observable<Producto[]> {
+    return this.http.get<Producto[]>(this.apiUrl);
+  }
+}
