8e7f6764809e570af15f58fc561cf6e345a83889
Ignacio Cortez <ig.cortez@duocuc.cl>
2026-07-17 18:45:36 -0400

    Update header.ts
    fix header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],   // ← AGREGAR ESTO
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
