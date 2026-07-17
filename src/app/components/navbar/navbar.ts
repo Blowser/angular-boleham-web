9fed284e52d558e28d8f0a08fb7961dd511cffb7
Ignacio Cortez <ig.cortez@duocuc.cl>
2026-07-17 18:55:15 -0400

    Update navbar.ts
    se adapta para evitar problemas con angular 17+

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],   // ← AGREGAR ESTO
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
 export class Navbar {
-
   constructor(public auth: AuthService) {}
-
 }
