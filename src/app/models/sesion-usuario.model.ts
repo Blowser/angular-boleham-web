dde6b8ee614d9e5e3e19a4fe098abb156c9fc9ee
Ignacio Cortez <ig.cortez@duocuc.cl>
2026-07-17 18:30:11 -0400

    Update sesion-usuario.model.ts
    se conecta auth

--- src/app/models/sesion-usuario.model.ts
+++ src/app/models/sesion-usuario.model.ts
@@ -3,6 +3,7 @@ import { RolUsuario } from './usuario.model';
 export interface SesionUsuario {
   id: number;
   nombreCompleto: string;
+  nombreUsuario: string;   // ← AGREGAR ESTO
   correo: string;
   rol: RolUsuario;
   logueado: true;
