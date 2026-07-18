3e2765104f8fb35396b925443a2a802ed7f93541
Ignacio Cortez <ig.cortez@duocuc.cl>
2026-07-17 21:13:06 -0400

    Update plushies-figuras.ts
    se integra plushies y figuras con api

--- src/app/pages/categorias/plushies-figuras/plushies-figuras.ts
+++ src/app/pages/categorias/plushies-figuras/plushies-figuras.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+
+import { ProductosService } from '../../../services/productos.service';
+import { Producto } from '../../../models/producto.model';
 
 @Component({
   selector: 'app-plushies-figuras',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './plushies-figuras.html',
   styleUrl: './plushies-figuras.scss'
 })
-export class PlushiesFiguras {}
+export class PlushiesFiguras implements OnInit {
+
+  productos: Producto[] = [];
+
+  constructor(private productosService: ProductosService) {}
+
+  ngOnInit(): void {
+    this.productosService.obtenerProductos().subscribe(data => {
+      this.productos = data.filter(p => p.categoria === 'PlushiesFiguras');
+    });
+  }
+}
