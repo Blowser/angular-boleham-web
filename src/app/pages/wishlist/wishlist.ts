b8ec86b8b170637e922268ea5a20c8c0ef46da19
Ignacio Cortez <ig.cortez@duocuc.cl>
2026-07-18 18:14:28 -0400

    Update wishlist.ts
    integrando wishlist y carrito

--- src/app/pages/wishlist/wishlist.ts
+++ src/app/pages/wishlist/wishlist.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+import { WishlistService } from '../../services/wishlist.service';
+import { Producto } from '../../models/producto.model';
 
 @Component({
   selector: 'app-wishlist',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './wishlist.html',
-  styleUrl: './wishlist.scss',
+  styleUrl: './wishlist.scss'
 })
-export class Wishlist {}
+export class Wishlist {
+
+  constructor(public wishlist: WishlistService) {}
+
+  obtenerItems(): Producto[] {
+    return this.wishlist.obtenerItems();
+  }
+
+  eliminar(id: number): void {
+    this.wishlist.eliminar(id);
+  }
+}
