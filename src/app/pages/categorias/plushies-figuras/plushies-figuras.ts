462746128155ad154954af402efe49ff12d66734
Ignacio Cortez <ig.cortez@duocuc.cl>
2026-07-18 18:33:51 -0400

    Update plushies-figuras.ts
    habilitando carrito y wishlist

--- src/app/pages/categorias/plushies-figuras/plushies-figuras.ts
+++ src/app/pages/categorias/plushies-figuras/plushies-figuras.ts
@@ -4,6 +4,8 @@ import { RouterLink } from '@angular/router';
 
 import { ProductosService } from '../../../services/productos.service';
 import { Producto } from '../../../models/producto.model';
+import { CarritoService } from '../../../services/carrito.service';
+import { WishlistService } from '../../../services/wishlist.service';
 
 @Component({
   selector: 'app-plushies-figuras',
@@ -16,11 +18,25 @@ export class PlushiesFiguras implements OnInit {
 
   productos: Producto[] = [];
 
-  constructor(private productosService: ProductosService) {}
+  constructor(
+    private productosService: ProductosService,
+    private carrito: CarritoService,
+    private wishlist: WishlistService
+  ) {}
 
   ngOnInit(): void {
     this.productosService.obtenerProductos().subscribe(data => {
       this.productos = data.filter(p => p.categoria === 'PlushiesFiguras');
     });
+  }
+
+  agregarAlCarrito(producto: Producto): void {
+    this.carrito.agregar(producto);
+    console.log('🛒 Producto agregado:', producto.nombre);
+  }
+
+  agregarWishlist(producto: Producto): void {
+    this.wishlist.agregar(producto);
+    console.log('💖 Wishlist → agregado:', producto.nombre);
   }
 }
