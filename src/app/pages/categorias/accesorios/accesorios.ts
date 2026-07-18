f343a803e6788695285c5ba194e10d3883a88549
Ignacio Cortez <ig.cortez@duocuc.cl>
2026-07-17 21:19:25 -0400

    Update accesorios.ts
    se integra accesorios con api

--- src/app/pages/categorias/accesorios/accesorios.ts
+++ src/app/pages/categorias/accesorios/accesorios.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+
+import { ProductosService } from '../../../services/productos.service';
+import { Producto } from '../../../models/producto.model';
 
 @Component({
   selector: 'app-accesorios',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './accesorios.html',
   styleUrl: './accesorios.scss'
 })
-export class Accesorios {}
+export class Accesorios implements OnInit {
+
+  productos: Producto[] = [];
+
+  constructor(private productosService: ProductosService) {}
+
+  ngOnInit(): void {
+    this.productosService.obtenerProductos().subscribe(data => {
+      this.productos = data.filter(p => p.categoria === 'Accesorios');
+    });
+  }
+}
