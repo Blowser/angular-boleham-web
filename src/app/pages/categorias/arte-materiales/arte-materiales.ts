faa1058e2b7b5c60cd1da253215f45e2548dc1b8
Ignacio Cortez <ig.cortez@duocuc.cl>
2026-07-18 18:14:51 -0400

    Update arte-materiales.ts
    integrando wishlist y carrito

--- src/app/pages/categorias/arte-materiales/arte-materiales.ts
+++ src/app/pages/categorias/arte-materiales/arte-materiales.ts
@@ -4,6 +4,8 @@ import { RouterLink } from '@angular/router';
 
 import { ProductosService } from '../../../services/productos.service';
 import { Producto } from '../../../models/producto.model';
+import { CarritoService } from '../../../services/carrito.service';
+import { WishlistService } from '../../../services/wishlist.service';
 
 @Component({
   selector: 'app-arte-materiales',
@@ -16,11 +18,25 @@ export class ArteMateriales implements OnInit {
 
   productos: Producto[] = [];
 
-  constructor(private productosService: ProductosService) {}
+  constructor(
+    private productosService: ProductosService,
+    private carrito: CarritoService,
+    private wishlist: WishlistService
+  ) {}
 
   ngOnInit(): void {
     this.productosService.obtenerProductos().subscribe(data => {
       this.productos = data.filter(p => p.categoria === 'ArteMateriales');
     });
+  }
+
+  agregarAlCarrito(producto: Producto): void {
+    this.carrito.agregar(producto);
+    console.log('🛒 Producto agregado:', producto.nombre);
+  }
+
+  agregarWishlist(producto: Producto): void {
+    this.wishlist.agregar(producto);
+    console.log('💖 Wishlist → agregado:', producto.nombre);
   }
 }
