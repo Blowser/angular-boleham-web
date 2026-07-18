b1425a52b0d757ab499ecbc861eea27db66b7853
Ignacio Cortez <ig.cortez@duocuc.cl>
2026-07-17 20:43:41 -0400

    Update comics-libros.ts
    integracion de categoria de comics y libros

--- src/app/pages/categorias/comics-libros/comics-libros.ts
+++ src/app/pages/categorias/comics-libros/comics-libros.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+
+import { ProductosService } from '../../../services/productos.service';
+import { Producto } from '../../../models/producto.model';
 
 @Component({
   selector: 'app-comics-libros',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './comics-libros.html',
   styleUrl: './comics-libros.scss'
 })
-export class ComicsLibros {}
+export class ComicsLibros implements OnInit {
+
+  productos: Producto[] = [];
+
+  constructor(private productosService: ProductosService) {}
+
+  ngOnInit(): void {
+    this.productosService.obtenerProductos().subscribe(data => {
+      this.productos = data.filter(p => p.categoria === 'ComicsLibros');
+    });
+  }
+}
