36bbd5623a7ca70bc6624b43a6735dc446341cd2
Ignacio Cortez <ig.cortez@duocuc.cl>
2026-07-17 21:07:46 -0400

    Update arte-materiales.ts
    se integra arte y materiales con api

--- src/app/pages/categorias/arte-materiales/arte-materiales.ts
+++ src/app/pages/categorias/arte-materiales/arte-materiales.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+
+import { ProductosService } from '../../../services/productos.service';
+import { Producto } from '../../../models/producto.model';
 
 @Component({
   selector: 'app-arte-materiales',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './arte-materiales.html',
   styleUrl: './arte-materiales.scss'
 })
-export class ArteMateriales {}
+export class ArteMateriales implements OnInit {
+
+  productos: Producto[] = [];
+
+  constructor(private productosService: ProductosService) {}
+
+  ngOnInit(): void {
+    this.productosService.obtenerProductos().subscribe(data => {
+      this.productos = data.filter(p => p.categoria === 'ArteMateriales');
+    });
+  }
+}
