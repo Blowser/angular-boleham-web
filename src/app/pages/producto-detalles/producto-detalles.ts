8f2648f6912cd1b45c18b84b169d41767a5eb530
Ignacio Cortez <ig.cortez@duocuc.cl>
2026-07-18 21:09:13 -0400

    Update producto-detalles.ts
    se crea productos preview y se modifica producto-detalles

--- src/app/pages/producto-detalles/producto-detalles.ts
+++ src/app/pages/producto-detalles/producto-detalles.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute, RouterLink } from '@angular/router';
 
 import { ProductosService } from '../../services/productos.service';
 import { Producto } from '../../models/producto.model';
+import { CarritoService } from '../../services/carrito.service';
+import { WishlistService } from '../../services/wishlist.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-producto-detalles',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './producto-detalles.html',
-  styleUrl: './producto-detalles.scss',
-  changeDetection: ChangeDetectionStrategy.Default
+  styleUrl: './producto-detalles.scss'
 })
 export class ProductoDetalles implements OnInit {
 
@@ -19,16 +21,33 @@ export class ProductoDetalles implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private productosService: ProductosService
+    private productosService: ProductosService,
+    private carrito: CarritoService,
+    private wishlist: WishlistService,
+    public auth: AuthService
   ) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      const id = Number(params.get('id'));
+    const id = Number(this.route.snapshot.paramMap.get('id'));
 
-      this.productosService.obtenerProducto(id).subscribe(data => {
-        this.producto = data;
-      });
+    this.productosService.obtenerProductos().subscribe(data => {
+      this.producto = data.find(p => p.id === id)!;
     });
+  }
+
+  agregarCarrito(): void {
+    if (!this.auth.estaLogueado()) {
+      alert('Debes iniciar sesión para agregar al carrito.');
+      return;
+    }
+    this.carrito.agregar(this.producto);
+  }
+
+  agregarWishlist(): void {
+    if (!this.auth.estaLogueado()) {
+      alert('Debes iniciar sesión para usar la wishlist.');
+      return;
+    }
+    this.wishlist.agregar(this.producto);
   }
 }
