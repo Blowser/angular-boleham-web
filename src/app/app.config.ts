3a5525410c2b599b573e87c5470d24efbe96baae
Ignacio Cortez <ig.cortez@duocuc.cl>
2026-07-18 23:36:30 -0400

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,13 +4,11 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(
-      routes,
-      withComponentInputBinding()   // ⭐ ESTA LÍNEA ES LA QUE ARREGLA EL BUG
-    ),
+    provideRouter(routes),
     provideHttpClient(withFetch())
   ]
 };
